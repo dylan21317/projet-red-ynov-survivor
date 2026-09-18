e21b6786b34e3f113bf55dbf18e22f0755ab35c1
fdylan <dylan.fiau@ynov.com>
2026-09-18 14:14:32 +0200

    ajout items

--- character.go
+++ character.go
@@ -11,3 +11,31 @@ type Character struct {
 	Stuff bool   // Bloque la salle 1 si 'false'
 	Inventory       []Item // Liste des objets ramassés
 }
+
+type Item struct {
+	Name        string
+	Type        string // "Arme", "Armure", "Potion"
+	BonusHP     int
+	BonusDamage int
+}
+
+// Crée un nouveau joueur
+func NewCharacter(name string) Character {
+	return Character{
+		Name:      name,
+		HP:        100,
+		MaxHP:     100,
+		Damage:    10,
+		Stuff:     false, // Bloqué au début
+		Inventory: []Item{},
+	}
+}
+
+// Récupère le stuff au vestiaire
+func (c *Character) EquipStarterStuff() {
+	c.Stuff = true
+	c.MaxHP += 20
+	c.HP = c.MaxHP
+	c.Damage += 5
+	fmt.Println("\n✓ Équipement de départ récupéré ! (PV +30, Dégâts +5)")
+}
